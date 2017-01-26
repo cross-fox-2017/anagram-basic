35da7604b79a3f893285af9c8e9ee88eb4109dde
Your Name <name@example.com>
2017-01-26 07:43:02 +0700

    Callback anagram Complete

--- helpers/util.js
+++ helpers/util.js
@@ -1,19 +1,33 @@
 'use strict';
-
-var models = require('../models');
+var express = require('express');
+var router = express.Router();
+var models = require('../models/index');
 var util = {};
 
-util.anagrams = function(callback){
-  // var hasil = ""
-  // models.Tb_anagram.findAll().then(function(dataAnagram){
-  //     dataAnagram.forEach(function(showdataAnagram){
-  //        return callback(showdataAnagram)
-  //     })
-  // })
+util.anagrams = function(word, callback){
+  models.Tb_anagram.findAll().then(function(dataAnagram) {
+      var data = []
+      dataAnagram.forEach(function(showdataAnagram) {
+          var status = true;
+          if (showdataAnagram.words.length == word.length) {
+              for (var i = 0; i < showdataAnagram.words.length; i++) {
+                  var arr1 = showdataAnagram.words.split("")
+                  var arr2 = word.split("")
+                  console.log(arr1);
+                  console.log(arr2);
+                  if (arr1.indexOf(arr2[i]) == -1) {
+                      status = false
+                      break;
+                  }
+              }
+              if (status) {
+                  data.push(showdataAnagram.words)
+              }
+          }
+      })
+      return callback(word, data)
+  })
 
-    // let  test = "sdfsd"
-    // callback(test)
-    // return ["asdf","test"]
 }
 
 module.exports = util;
--- routes/index.js
+++ routes/index.js
@@ -12,43 +12,43 @@ router.get('/', function(req, res, next) {
     // res.render('index', {title: 'Anagrams', word: word, anagrams: {} });
 
     // release 3
-    models.Tb_anagram.findAll().then(function(dataAnagram) {
-        var data = []
-        dataAnagram.forEach(function(showdataAnagram) {
-            var status = true;
-            if (showdataAnagram.words.length == word.length) {
-                for (var i = 0; i < showdataAnagram.words.length; i++) {
-                    var arr1 = showdataAnagram.words.split("")
-                    var arr2 = word.split("")
-                    console.log(arr1);
-                    console.log(arr2);
-                    if (arr1.indexOf(arr2[i]) == -1) {
-                        status = false
-                        break;
-                    }
-                }
-                if (status) {
-                    data.push(showdataAnagram.words)
-                }
-            }
-        })
-        res.render('index', {
-            title: 'Anagrams',
-            word: word,
-            anagrams: data
-        });
-    })
-
-
-
-
-    // helper.anagrams(function(data) {
+    // models.Tb_anagram.findAll().then(function(dataAnagram) {
+    //     var data = []
+    //     dataAnagram.forEach(function(showdataAnagram) {
+    //         var status = true;
+    //         if (showdataAnagram.words.length == word.length) {
+    //             for (var i = 0; i < showdataAnagram.words.length; i++) {
+    //                 var arr1 = showdataAnagram.words.split("")
+    //                 var arr2 = word.split("")
+    //                 console.log(arr1);
+    //                 console.log(arr2);
+    //                 if (arr1.indexOf(arr2[i]) == -1) {
+    //                     status = false
+    //                     break;
+    //                 }
+    //             }
+    //             if (status) {
+    //                 data.push(showdataAnagram.words)
+    //             }
+    //         }
+    //     })
     //     res.render('index', {
     //         title: 'Anagrams',
     //         word: word,
     //         anagrams: data
     //     });
-    // });
+    // })
+
+
+
+
+    helper.anagrams(word,function(source,data) {
+        res.render('index', {
+            title: 'Anagrams',
+            word: word,
+            anagrams: data
+        });
+    });
 });
 
 module.exports = router;
